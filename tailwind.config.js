1d459947e2229c52641fd2a8c9070d822e888d92
Dustin <drumcrazy123@gmail.com>
2025-05-03 23:35:29 -0600

    Added Flowbite and GitHub Actions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+const flowbite = require('flowbite/plugin');
+
 module.exports = {
   content: [
-    "./layouts/**/*.html",
-    "./content/**/*.md",
-    "./assets/**/*.js",
+    './layouts/**/*.{html,js}',
+    './content/**/*.{html,md}',
+    './assets/**/*.{js,css}',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [flowbite],
+}
